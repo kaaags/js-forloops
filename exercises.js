ec6455350770a2b264a0f738711272947367731c
Sean Kagawa <seankagawa@gmail.com>
2018-02-15 05:28:57 -1000

    compl ex3-A

--- exercises.js
+++ exercises.js
@@ -56,6 +56,16 @@ for(var i = 0; i < presidents.length; i++){
 
 console.log(presidents.length);
 
+//3-A)
+
+function leaders(person){
+  for(var i = 0; i < presidents.length; i++){
+    console.log("President " + person[i] + " was a great leader.");
+  }
+}
+
+leaders(presidents);
+
 /* 4) Line Number
 Declare a variable named `stringOfNumbers` and assign its value to an empty string. 
 
